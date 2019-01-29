fb9cf6bf8bf8a55b7ce5a48f7eeb907ae16b3b42
davifurtado <44845457+davifurtado@users.noreply.github.com>
2019-01-29 10:50:47 -0200

    adding files

---
+++ build/precache-manifest.85aea46bb4473274c0ba8850b64bccf6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8c2faa9b7b58c5156969",
+    "url": "/static/js/main.8c2faa9b.chunk.js"
+  },
+  {
+    "revision": "fd69494ba6e526ef3c3e",
+    "url": "/static/js/1.fd69494b.chunk.js"
+  },
+  {
+    "revision": "8c2faa9b7b58c5156969",
+    "url": "/static/css/main.03909800.chunk.css"
+  },
+  {
+    "revision": "fd69494ba6e526ef3c3e",
+    "url": "/static/css/1.35f092b5.chunk.css"
+  },
+  {
+    "revision": "68e5446314d05aea2012cde371ffdb41",
+    "url": "/index.html"
+  }
+];
---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll.js';
+import './App.css';
+
+
+class App extends Component{
+	constructor(){
+		super()
+		this.state = {
+			robots: [],
+			searchfield: ''
+		}
+	}
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response=> response.json())
+			.then(users => {this.setState({ robots: users})});
+	}
+
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value })
+		const filteredRobots = this.state.robots.filter(robot => {
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+		})
+	}
+
+	render() {
+			const filteredRobots = this.state.robots.filter(robot => {
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+		})
+			if (this.state.robots.lenght === 0) {
+				return <h1>Loading</h1>
+			} else {
+		return (
+	   	<div className='tc'>
+			<h1 className='f2'>RoboFriends</h1>
+			<SearchBox searchChange={this.onSearchChange}/>
+			<Scroll>
+				<CardList robots={filteredRobots} />
+			</Scroll>
+	   	</div>
+		);
+}
+}
+}
+export default App;
